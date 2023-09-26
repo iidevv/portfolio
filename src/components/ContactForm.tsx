aa0f479b01cdd26e48a5b966260afe152cdcdd1e
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-09-26 15:58:23 -0700

    portfolio

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, { useState, FormEvent } from 'react';
+
+interface FormData {
+    name: string;
+    email: string;
+    message: string;
+}
+
+const ContactForm: React.FC = () => {
+    const [formData, setFormData] = useState<FormData>({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        // Handle form submission logic here
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    );
+}
+
+export default ContactForm;
---
+++ src/components/Skills.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Skills: React.FC = () => {
+    const skills = [
+        {
+            title: "Languages & Technologies",
+            items: ["HTML, CSS (SCSS, BEM)", "JavaScript (ES6+)", "TypeScript"]
+        },
+        {
+            title: "Frameworks & Libraries",
+            items: ["React.js", "Redux", "Tailwind CSS"]
+        },
+        {
+            title: "Backend",
+            items: ["Node.js/Express.js", "PHP",]
+        },
+        {
+            title: "Databases",
+            items: ["MongoDB", "MySQL"]
+        },
+        {
+            title: "API",
+            items: ["RESTful API"]
+        },
+    ]
+    return (
+        <div className="skills">
+            {skills.map((skillSection, index) => (
+                <div className="skills__section" key={index}>
+                    <h3 className="skills__title">{skillSection.title}:</h3>
+                    <ul className="skills__items">
+                        {skillSection.items && skillSection.items.map((item, itemIndex) => (
+                            <li className="skills__item" key={itemIndex}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Skills;
