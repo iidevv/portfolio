c2c9a929672a5221dae31aa040f9e6a73e1240c5
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-10-05 12:57:59 -0700

    analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './styles/globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
-import Header from '../components/Header';
 import Providers from './providers';
+import { Analytics } from '@vercel/analytics/react';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const roboto = Roboto({
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Analytics />
           <Footer />
         </Providers>
       </body>
